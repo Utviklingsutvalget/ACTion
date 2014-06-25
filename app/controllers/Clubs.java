2efd7c0d91977b926dadd884b402e51c760dc5fd
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-06-25 15:22:26 +0200

    Add finder plus seed for locations

--- app/controllers/Clubs.java
+++ app/controllers/Clubs.java
@@ -1,15 +1,27 @@
 package controllers;
 
 import models.Club;
+import models.Location;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Clubs extends Controller {
     public static Result index() {
         final List<Club> clubs = Club.find.all();
+        List<Location> locations = Location.find.all();
+        if(locations == null || locations.isEmpty()) {
+            locations = new ArrayList<>();
+            for(int i = 0 ; i < 3 ; i++) {
+                Location location = new Location();
+                location.name = "Location"+i;
+                Location.update(location);
+                locations.add(location);
+            }
+        }
         return ok(views.html.club.index.render(clubs));
     }
 
--- app/models/Location.java
+++ app/models/Location.java
@@ -1,7 +1,9 @@
 package models;
 
+import com.avaje.ebean.Ebean;
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
+import play.db.ebean.Transactional;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +11,11 @@ import javax.persistence.Id;
 @Entity
 public class Location extends Model {
     public static Finder<Long, Location> find = new Finder<>(Long.class, Location.class);
+
+    @Transactional
+    public static void update(Location location) {
+        Ebean.update(location);
+    }
 
     @Id
     public long id;
