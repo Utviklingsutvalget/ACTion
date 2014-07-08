05379de688507976f4272fca4bbb7735ba330a11
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-07-08 17:16:29 +0200

    Migrate PowerupFactory to Powerup.

--- app/powerups/Powerup.java
+++ app/powerups/Powerup.java
@@ -1,9 +1,12 @@
 package powerups;
 
+import models.Activation;
 import models.Club;
 import play.twirl.api.Html;
 
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public abstract class Powerup implements Serializable {
 
@@ -14,4 +17,19 @@ public abstract class Powerup implements Serializable {
     }
 
     public abstract Html render();
+
+    public static Powerup getPowerup(Activation activation) {
+        models.Powerup powerupModel = models.Powerup.find.byId(activation.key.powerupId);
+        Club club = Club.find.byId(activation.key.clubId);
+
+        try {
+            @SuppressWarnings("unchecked")
+            Class<? extends Powerup> c = (Class<? extends Powerup>) Class.forName(powerupModel.className);
+            Constructor<? extends Powerup> constructor = c.getDeclaredConstructor(Club.class);
+            return constructor.newInstance(club);
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
