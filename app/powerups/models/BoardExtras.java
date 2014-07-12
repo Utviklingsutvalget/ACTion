6b937880bd2a696d2249eca38ca8a47b34546ba7
biefre13 <slappyjackz@gmail.com>
2014-07-13 00:23:34 +0200

    swapped membership for user

--- app/powerups/models/BoardExtras.java
+++ app/powerups/models/BoardExtras.java
@@ -11,15 +11,34 @@ import javax.persistence.*;
 @Entity
 public class BoardExtras extends Model {
 
+    @EmbeddedId
+    public BoardKey key;
+
     @ManyToOne
     public Board board;
 
     @Constraints.Required
     @OneToOne
     @PrimaryKeyJoinColumn
-    public Membership member;
+    public User member;
 
     @Constraints.Required
     public String title;
+
+    @Embeddable
+    public class BoardKey {
+
+        public Long clubId;
+
+        public Integer extrasId;
+
+        public boolean equals(Object other) {
+            return other == this || other instanceof BoardKey && ((BoardKey) other).clubId.equals(this.clubId);
+        }
+
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
 
 }
