3b74fbbfb2742ad8e69c002e2f98fb8fff50e559
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-07-29 17:25:21 +0200

    Reconfigure CKEditor, replace skin mooonodark with moonocolor

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -30,6 +30,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
+    config.skin = 'moonocolor';
+
+    config.removePlugins = 'elementspath,inlinesave,inlinecancel,font';
+
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
