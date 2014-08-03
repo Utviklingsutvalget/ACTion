6c2268d1b2ae633d565ba6442d7c9b66f5bc0355
biefre13 <slappyjackz@gmail.com>
2014-08-03 21:12:15 +0200

    Feed now contains two lists that is sent to view, both of which is necessary to display updated design changes.
    distribution from main feedlist is divided according to how view is supposed to display its contents. A total of 14 feedposts divided into one group of 2 posts, and
    one group of the remaining 12 posts.
    
    FeedPowerup is slightly changed but keeps previous design

--- app/controllers/Feeds.java
+++ app/controllers/Feeds.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.*;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.Authorize;
@@ -14,6 +15,7 @@ public class Feeds extends Controller {
 
     //private static final int MAXIMUMFEEDSIZE = 30;
     private static final int MAXFEEDSPERCLUB = 5;
+    private static final int MAXINDEXFEEDSIZE = 14;
 
     /*
     * has yet to be implemented in routes file
@@ -22,6 +24,8 @@ public class Feeds extends Controller {
     public static Result index(){
 
         List<Feed> feedList = new ArrayList<>();
+        List<Feed> initialList = new ArrayList<>();
+        List<Feed> remainingList = new ArrayList<>();
 
         // TODO FIND A MORE EFFECTIVE IMPLEMENTATION LATER
 
@@ -49,12 +53,32 @@ public class Feeds extends Controller {
             return unauthorized("You need to be logged in to see feed");
         }
 
-        if(feedList != null){
+        if(feedList != null && !feedList.isEmpty()){
             feedList.sort(new FeedSorter());
             Collections.reverse(feedList);
+            Logger.info(feedList.size() + " is feedList size");
+
+            //Adds correct entries to correct lists, up to 10 entries
+            for(int i = 0; i < feedList.size(); i++){
+
+                if(i < 2){
+
+                    initialList.add(feedList.get(i));
+                    Logger.info("added to initial " + i);
+
+                }else if(i < MAXINDEXFEEDSIZE && i >= 2){
+
+                    remainingList.add(feedList.get(i));
+                    Logger.info("added to remaining " + i);
+
+                }else{
+                    Logger.info("Max of 14 has been reached or list has no more entries, terminating");
+                    break;
+                }
+            }
         }
 
-        return ok(views.html.feed.index.render(feedList));
+        return ok(views.html.feed.index.render(remainingList, initialList));
     }
 
     //Find up x feeds by a give club (until maxfeedsperclub is reached)
