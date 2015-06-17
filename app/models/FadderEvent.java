bb63dff6af8bf80e95df86174cfe54b0c2b95168
bromle <espenbang@gmail.com>
2015-06-17 20:20:48 +0200

    Created classes for Faddermodul (FadderEvent and FadderPlan

---
+++ app/models/FadderEvent.java
@@ -0,0 +1,69 @@
+package models;
+
+import play.data.validation.Constraints;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+public class FadderEvent {
+    @Id
+    private Long id;
+    @Constraints.Required
+    private LocalDateTime time;
+    @Constraints.Required
+    private String title;
+    @Constraints.Required
+    @Column (length = 300)
+    private String description;
+    @Constraints.Required
+    private String location;
+
+    public FadderEvent(Long id, LocalDateTime time, String title, String description, String location) {
+        this.id = id;
+        this.time= time;
+        this.title= title;
+        this.description= description;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ app/models/FadderPlan.java
@@ -0,0 +1,49 @@
+package models;
+
+import java.time.Year;
+import java.util.List;
+
+public class FadderPlan {
+    private Long id;
+    private String campus;
+    private List<FadderEvent> events;
+    private Year year;
+
+    public FadderPlan(Long id, String campus, List<FadderEvent> events, Year year) {
+        this.campus = campus;
+        this.events = events;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public List<FadderEvent> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<FadderEvent> events) {
+        this.events = events;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+}
