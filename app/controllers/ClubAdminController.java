4f89009104c6e5bdf36389f0945254ff9e81365c
eivindveg <eivindveg@hotmail.com>
2015-06-27 14:56:31 +0200

    move club admin into new view

--- app/controllers/ClubAdminController.java
+++ app/controllers/ClubAdminController.java
@@ -1,13 +1,28 @@
 package controllers;
 
+import models.Location;
+import models.clubs.Club;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Content;
+import services.ClubService;
+import services.LocationService;
 import views.html.admin.clubs.index;
+
+import javax.inject.Inject;
+import java.util.List;
 
 public class ClubAdminController extends Controller {
 
+    @Inject
+    private ClubService clubService;
+
+    @Inject
+    private LocationService locationService;
+
     public Result index() {
-        return ok((Content) index.render());
+        List<Location> locations = locationService.findAll();
+        List<Club> clubs = clubService.findAll();
+        return ok((Content) index.render(clubs, locations));
     }
 }
