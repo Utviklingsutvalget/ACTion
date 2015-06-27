5a2e657fa8485ef425154b9e41028f0ace75291a
eivindveg <eivindveg@hotmail.com>
2015-06-27 14:51:00 +0200

    add empty club admin view

--- app/controllers/ClubAdminController.java
+++ app/controllers/ClubAdminController.java
@@ -2,10 +2,12 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.twirl.api.Content;
+import views.html.admin.clubs.index;
 
 public class ClubAdminController extends Controller {
 
     public Result index() {
-        return TODO;
+        return ok((Content) index.render());
     }
 }
