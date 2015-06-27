ac923882db5679617fa6a12bef01db8641bdcbed
eivindveg <eivindveg@hotmail.com>
2015-06-27 16:48:51 +0200

    update club model further

--- app/controllers/ClubAdminController.java
+++ app/controllers/ClubAdminController.java
@@ -2,7 +2,9 @@ package controllers;
 
 import models.Location;
 import models.clubs.Club;
+import play.Logger;
 import play.data.Form;
+import play.data.validation.ValidationError;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Content;
@@ -13,6 +15,7 @@ import views.html.admin.clubs.index;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Map;
 
 public class ClubAdminController extends Controller {
 
@@ -36,6 +39,31 @@ public class ClubAdminController extends Controller {
     }
 
     public Result create() {
-        return TODO;
+        List<Location> locations = locationService.findAll();
+        Form<Club> form = Form.form(Club.class).bindFromRequest(request());
+        Map<String, String> data = form.data();
+        Logger.warn("Received following data: " + data);
+        discardError(form, "location");
+        Club club = form.get();
+
+        String locationId = data.get("location");
+        club.setLocation(getLocationFromStringId(locations, locationId));
+
+        if(form.hasErrors()) {
+            return ok(create.render(locations, form));
+        }
+        clubService.save(club);
+        return redirect(routes.Clubs.show(club.getId()));
+    }
+
+    private void discardError(final Form<?> form, String field) {
+        List<ValidationError> location = form.errors().remove(field);
+    }
+
+    private Location getLocationFromStringId(final List<Location> locations, final String locationId) {
+        return locations.stream()
+                .filter(loc -> loc.getId() == Long .parseLong(locationId))
+                .findFirst()
+                .orElseGet(null);
     }
 }
--- app/models/clubs/Club.java
+++ app/models/clubs/Club.java
@@ -6,6 +6,7 @@ import models.Event;
 import models.Feed;
 import models.Location;
 import models.Membership;
+import play.data.validation.Constraints;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -29,8 +30,16 @@ public class Club {
     private List<Event> events = new ArrayList<>();
     @OneToMany(cascade = CascadeType.REMOVE)
     private List<Feed> feedPosts = new ArrayList<>();
-    @OneToOne(mappedBy = "club")
-    private ClubInfo info = new ClubInfo();
+    @Constraints.Required
+    @Constraints.MaxLength(10000)
+    private String description;
+    @Constraints.Required
+    @Constraints.MaxLength(300)
+    private String listDescription;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<BoardMembership> boardMemberships;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Pending> pendingMemberships;
 
     public Club(String name, String shortName, Location location) {
         this.name = name;
@@ -43,12 +52,36 @@ public class Club {
         Ebean.update(club);
     }
 
-    public ClubInfo getInfo() {
-        return info;
+    public String getDescription() {
+        return description;
     }
 
-    public void setInfo(final ClubInfo info) {
-        this.info = info;
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getListDescription() {
+        return listDescription;
+    }
+
+    public void setListDescription(final String listDescription) {
+        this.listDescription = listDescription;
+    }
+
+    public List<BoardMembership> getBoardMemberships() {
+        return boardMemberships;
+    }
+
+    public void setBoardMemberships(final List<BoardMembership> boardMemberships) {
+        this.boardMemberships = boardMemberships;
+    }
+
+    public List<Pending> getPendingMemberships() {
+        return pendingMemberships;
+    }
+
+    public void setPendingMemberships(final List<Pending> pendingMemberships) {
+        this.pendingMemberships = pendingMemberships;
     }
 
     public String getName() {
